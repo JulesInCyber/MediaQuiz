9d95774094bdf3a6e3e50d46351115719197401c
Julian@Legion <julianjung1992@icloud.com>
2026-09-23 19:25:03 +0200

    Script for creating tables

---
+++ data/tables.sql
@@ -0,0 +1,46 @@
+-- Creating tables
+CREATE TABLE media (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    release_year INTEGER,
+    type TEXT NOT NULL
+);
+
+CREATE TABLE people (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE genres (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE media_people (
+    media_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    role TEXT NOT NULL,
+
+    PRIMARY KEY (media_id, person_id, role),
+
+    FOREIGN KEY (media_id) REFERENCES media(id),
+    FOREIGN KEY (person_id) REFERENCES people(id)
+);
+
+CREATE TABLE media_genres (
+    media_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+
+    PRIMARY KEY (media_id, genre_id),
+
+    FOREIGN KEY (media_id) REFERENCES media(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+CREATE TABLE quotes (
+    id INTEGER PRIMARY KEY,
+    media_id INTEGER NOT NULL,
+    quote TEXT NOT NULL,
+
+    FOREIGN KEY (media_id) REFERENCES media(id)
+);
