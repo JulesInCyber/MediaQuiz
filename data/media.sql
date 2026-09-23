dd156d741ac467e33f6b50901ed2671787e15cc6
Julian@Legion <julianjung1992@icloud.com>
2026-09-23 19:51:49 +0200

    Updated media.sql

--- data/media.sql
+++ data/media.sql
@@ -1,4 +1,5 @@
 INSERT INTO media (title, release_year, type)
 VALUES
-    ('The Matrix', 1999, 'movie')
+    ('The Matrix', 1999, 'movie'),
+    ('The Shawshank Redemption', 1993, 'movie')
 ON CONFLICT (title) DO NOTHING;
