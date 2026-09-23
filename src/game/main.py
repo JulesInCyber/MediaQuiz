3f7111451ecb66a1d3f93e2912c12742fe8ef5f5
Julian@Legion <julianjung1992@icloud.com>
2026-09-23 18:48:15 +0200

    Working Starting Point

--- src/game/main.py
+++ src/game/main.py
@@ -1,5 +1,8 @@
 from src.database.queries import random_media
 
-media = random_media()
+def main():
+    media = random_media()
+    print(media)
 
-print(media)
+if __name__ == "__main__":
+    main()
