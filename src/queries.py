ee92363dd794c257711ed4c182de0883f4136133
Julian@Legion <julianjung1992@icloud.com>
2026-09-23 17:37:30 +0200

    First Query with python

---
+++ src/queries.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def random_media():
+    connection = sqlite3.connect("data/quiz.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+                   SELECT title
+                   FROM media
+                   ORDER BY random()
+                   LIMIT 1;
+                   """)
+
+    media = cursor.fetchall()
+    connection.close()
+
+    return media 
+
+random_media = random_media()
+
+print(random_media[0])
