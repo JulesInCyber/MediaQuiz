7de8d3cfad6ad6c091ce99ac4388d48a732cdfe6
Julian@Legion <julianjung1992@icloud.com>
2026-09-23 19:37:20 +0200

    removed revert messages

--- data/tables.sql
+++ data/tables.sql
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-SQLite version 3.53.4 2026-07-24 19:02:57
-Enter ".help" for usage hints.
-[?2004hsqlite> ^C[?2004l[?2004h[C[C[C[C[C[C[C[C^C[?2004l
-=======
 -- Creating tables
 CREATE TABLE media (
     id INTEGER PRIMARY KEY,
@@ -49,4 +44,3 @@ CREATE TABLE quotes (
 
     FOREIGN KEY (media_id) REFERENCES media(id)
 );
->>>>>>> parent of e6c5012 (Updated to check existing tables)
