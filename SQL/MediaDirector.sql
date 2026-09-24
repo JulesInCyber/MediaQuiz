8b9da03fb4c509175229fa370d1bcb6801330bd2
Julian Jung <tick.73spatial@icloud.com>
2026-09-24 09:35:29 +0200

    Getting Director Info

--- SQL/MediaDirector.sql
+++ SQL/MediaDirector.sql
@@ -5,4 +5,4 @@ JOIN media_people
 JOIN media
     ON media.id = media_people.media_id
 WHERE media.title = ?
-    AND media_people.role = 'actor';
+    AND media_people.role = 'director';
--- src/database/queries.py
+++ src/database/queries.py
@@ -7,13 +7,36 @@ live_db = "data/quiz.db"
 def get_random_media():
     connection = sqlite3.connect(test_db)
     with open ("SQL/RandomMedia.sql", "r") as file:
-        sql_script = file.read()
+        query = file.read()
 
     cursor = connection.cursor()
-    cursor.execute(sql_script)
+    cursor.execute(query)
     connection.commit()
     random_media = cursor.fetchone()
     connection.close()
 
     return random_media
     
+def get_media_actors(media):
+    connection = sqlite3.connect(test_db)
+    with open ("SQL/MediaActors.sql", "r") as file:
+        query = file.read()
+
+    cursor = connection.cursor()
+    cursor.execute(query, (media,))
+    actors = cursor.fetchall()
+    connection.close()
+
+    return [actor[0] for actor in actors]
+
+def get_media_director(media):
+    connection = sqlite3.connect(test_db)
+    with open ("SQL/MediaDirector.sql") as file:
+        query = file.read()
+
+    cursor = connection.cursor()
+    cursor.execute(query, (media,))
+    directors = cursor.fetchall()
+    connection.close()
+
+    return [director[0] for director in directors]
