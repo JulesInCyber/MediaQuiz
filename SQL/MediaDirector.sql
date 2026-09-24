37db5d0c47526fb940bc6fbddf19721b4fb24c05
Julian Jung <tick.73spatial@icloud.com>
2026-09-24 09:32:19 +0200

    Query to get Director

---
+++ SQL/MediaDirector.sql
@@ -0,0 +1,8 @@
+SELECT people.name
+FROM people
+JOIN media_people
+    ON people.id = media_people.person_id
+JOIN media
+    ON media.id = media_people.media_id
+WHERE media.title = ?
+    AND media_people.role = 'actor';
