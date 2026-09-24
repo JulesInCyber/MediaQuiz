f977cf32d57b5a5dfbd83311a1c65391836cf52e
Julian Jung <tick.73spatial@icloud.com>
2026-09-24 10:00:07 +0200

    Function for Genres

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,11 +1,16 @@
-import random
 import sqlite3
 
 test_db = "testing/test.db"
 live_db = "data/quiz.db"
 
-def get_random_media():
+def connect_db():
+    # Change Database after Testing
     connection = sqlite3.connect(test_db)
+    return connection
+
+
+def get_random_media():
+    connection = connect_db()
     with open ("SQL/RandomMedia.sql", "r") as file:
         query = file.read()
 
@@ -18,7 +23,7 @@ def get_random_media():
     return random_media
     
 def get_media_actors(media):
-    connection = sqlite3.connect(test_db)
+    connection = connect_db()
     with open ("SQL/MediaActors.sql", "r") as file:
         query = file.read()
 
@@ -30,8 +35,8 @@ def get_media_actors(media):
     return [actor[0] for actor in actors]
 
 def get_media_director(media):
-    connection = sqlite3.connect(test_db)
-    with open ("SQL/MediaDirector.sql") as file:
+    connection = connect_db()
+    with open ("SQL/MediaDirector.sql", "r") as file:
         query = file.read()
 
     cursor = connection.cursor()
@@ -40,3 +45,15 @@ def get_media_director(media):
     connection.close()
 
     return [director[0] for director in directors]
+
+def get_media_genres(media):
+    connection = connect_db()
+    with open ("SQL/MediaGenres.sql", "r") as file:
+        query = file.read()
+
+    cursor = connection.cursor()
+    cursor.execute(query, (media,))
+    genres = cursor.fetchall()
+    connection.close()
+
+    return [genre[0] for genre in genres]
