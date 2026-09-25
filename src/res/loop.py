eefedf2fc6391ea601d9cbc9c15fece629559386
Julian Jung <tick.73spatial@icloud.com>
2026-09-25 10:46:33 +0200

    Exctracted and enumerated Clues

--- src/res/loop.py
+++ src/res/loop.py
@@ -15,17 +15,29 @@ def check_answer(answer, title):
 
     return result
 
-def make_guess():
+def get_clues():
     media = get_random_media()
 
+    media_id = media[0]
     media_title = media[1]
     media_release = media[2]
 
-    genres = get_media_genres(media)
-    director = get_media_director(media)
-    actors = get_media_actors(media)
+    genres = get_media_genres(media_id)
+    director = get_media_director(media_id)
+    actors = get_media_actors(media_id)
 
     clues = [
         f"Year of release was {media_release}",
         f"The media has the following genre(s): {', '.join(genres)}",
-        f"It was directed by {', '.join(directors)}"]
+        f"It was directed by {', '.join(director)}",
+        f"The actors {', '.join(actors)} are part of the cast.",
+        ]
+
+    return clues
+
+
+# all_clues = get_clues()
+#
+# for i, clue in enumerate(all_clues):
+#     print(i+1, clue)
+#
